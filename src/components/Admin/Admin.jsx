2f0501a918c025b5f6fada95299d61a0635531e7
deeksha81 <bajpaideeksha81@gmail.com>
2026-01-11 17:24:24 +0530

    Added History UI styling and layout improvements

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,9 +1,95 @@
 import React from 'react'
 import styles from './Admin.module.css';
+import Skeleton from '@mui/material/Skeleton';
 
 const Admin = () => {
   return (
-    <div>Admin</div>
+    <div className= {styles.Admin}>
+        <div className={styles.AdminBlock}>
+
+    <Skeleton
+        variants="rectangular"
+        width={266}
+        height={200}
+        sx={{borderRadius:"20px"}}
+        
+        
+        />
+
+            <div className= {styles.AdminCard}>
+                <h2>CodingHunger</h2>
+                  <br/>
+                <p style={{color:"blue"}}>mashhood@gmail.com</p>
+                   <br/>
+                <h3>Score: 50%</h3>
+                 <br/>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quia iste dolorem explicabo, odit impedit adipisci iure necessitatibus! Id, quidem dicta. Nostrum adipisci porro est eligendi reiciendis nesciunt vitae facere.</p>
+            </div>
+
+
+               <div className= {styles.AdminCard}>
+                <h2>CodingHunger</h2>
+                  <br/>
+                <p style={{color:"blue"}}>mashhood@gmail.com</p>
+                   <br/>
+                <h3>Score: 50%</h3>
+                 <br/>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quia iste dolorem explicabo, odit impedit adipisci iure necessitatibus! Id, quidem dicta. Nostrum adipisci porro est eligendi reiciendis nesciunt vitae facere.</p>
+            </div>
+
+
+
+               <div className= {styles.AdminCard}>
+                <h2>CodingHunger</h2>
+                  <br/>
+                <p style={{color:"blue"}}>mashhood@gmail.com</p>
+                   <br/>
+                <h3>Score: 50%</h3>
+                 <br/>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quia iste dolorem explicabo, odit impedit adipisci iure necessitatibus! Id, quidem dicta. Nostrum adipisci porro est eligendi reiciendis nesciunt vitae facere.</p>
+            </div>
+
+
+
+
+               <div className= {styles.AdminCard}>
+                <h2>CodingHunger</h2>
+                  <br/>
+                <p style={{color:"blue"}}>mashhood@gmail.com</p>
+                   <br/>
+                <h3>Score: 50%</h3>
+                 <br/>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quia iste dolorem explicabo, odit impedit adipisci iure necessitatibus! Id, quidem dicta. Nostrum adipisci porro est eligendi reiciendis nesciunt vitae facere.</p>
+            </div>
+
+
+
+               <div className= {styles.AdminCard}>
+                <h2>CodingHunger</h2>
+                  <br/>
+                <p style={{color:"blue"}}>mashhood@gmail.com</p>
+                   <br/>
+                <h3>Score: 50%</h3>
+                 <br/>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quia iste dolorem explicabo, odit impedit adipisci iure necessitatibus! Id, quidem dicta. Nostrum adipisci porro est eligendi reiciendis nesciunt vitae facere.</p>
+            </div>
+            
+
+
+
+
+
+
+
+        </div>
+
+
+
+
+
+
+
+    </div>
   )
 }
 
--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -1,9 +1,107 @@
 import React from 'react'
 import styles from './History.module.css';
+import Skeleton from '@mui/material/Skeleton';
 
 const History = () => {
   return (
-    <div>History</div>
+    <div className= {styles.History}>
+       
+      <div className= {styles.HistoryCardBlock}>
+
+        <Skeleton
+        variants="rectangular"
+        width={270}
+        height={250}
+        sx={{borderRadius:"20px"}}
+        
+        
+        />
+
+
+
+        <div className= {styles.HistoryCard}>
+
+          <div className= {styles.cardPercentage}>80%</div>
+               <br/>
+          <h2>Frontend Developer</h2>
+                 <br/>
+          <p>Resume Name: Resume.pdf</p>
+                  <br/>
+          
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed itaque explicabo exercitationem facilis. Impedit voluptatum eius pariatur dicta est, earum consectetur quis magnam maxime ut totam eligendi, esse repellat dolores.</p>
+               <br/>
+          <p>Dated : 2025-11-18</p>
+          </div>
+
+
+           <div className= {styles.HistoryCard}>
+
+          <div className= {styles.cardPercentage}>80%</div>
+               <br/>
+          <h2>Frontend Developer</h2>
+                 <br/>
+          <p>Resume Name: Resume.pdf</p>
+                  <br/>
+          
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed itaque explicabo exercitationem facilis. Impedit voluptatum eius pariatur dicta est, earum consectetur quis magnam maxime ut totam eligendi, esse repellat dolores.</p>
+               <br/>
+          <p>Dated : 2025-11-18</p>
+          </div>
+              
+        
+        
+
+         <div className= {styles.HistoryCard}>
+
+          <div className= {styles.cardPercentage}>80%</div>
+               <br/>
+          <h2>Frontend Developer</h2>
+                 <br/>
+          <p>Resume Name: Resume.pdf</p>
+                  <br/>
+          
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed itaque explicabo exercitationem facilis. Impedit voluptatum eius pariatur dicta est, earum consectetur quis magnam maxime ut totam eligendi, esse repellat dolores.</p>
+               <br/>
+          <p>Dated : 2025-11-18</p>
+          </div>
+
+          <div className= {styles.HistoryCard}>
+
+          <div className= {styles.cardPercentage}>80%</div>
+               <br/>
+          <h2>Frontend Developer</h2>
+                 <br/>
+          <p>Resume Name: Resume.pdf</p>
+                  <br/>
+          
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed itaque explicabo exercitationem facilis. Impedit voluptatum eius pariatur dicta est, earum consectetur quis magnam maxime ut totam eligendi, esse repellat dolores.</p>
+               <br/>
+          <p>Dated : 2025-11-18</p>
+          </div>
+           
+          <div className= {styles.HistoryCard}>
+
+          <div className= {styles.cardPercentage}>80%</div>
+               <br/>
+          <h2>Frontend Developer</h2>
+                 <br/>
+          <p>Resume Name: Resume.pdf</p>
+                  <br/>
+          
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed itaque explicabo exercitationem facilis. Impedit voluptatum eius pariatur dicta est, earum consectetur quis magnam maxime ut totam eligendi, esse repellat dolores.</p>
+               <br/>
+          <p>Dated : 2025-11-18</p>
+          </div>
+        
+        </div>
+        
+        
+        
+    </div>
+
+
+
+    
   )
 }
 
